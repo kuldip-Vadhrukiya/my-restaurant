1dfd017f75b9c89fb4705ba9ce71b81b325b15f2
vadhrukiya kuldip <vadhrukiyakuldip@gmail.com>
2026-03-12 17:12:52 +0530

    changed to sqlite for live

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -61,14 +61,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'restaurant.wsgi.application'
 
+# settings.py में इसे बदलें
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'restaurant_db',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
